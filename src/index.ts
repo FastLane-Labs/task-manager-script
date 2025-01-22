d8caebbd182bd166c8d6a25b2b85dc315209cb20
marshabl <marshabl12@gmail.com>
2025-01-22 09:54:15 -0800

    finish script

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
 import { encodeFunctionData, Hex } from "viem";
 import { taskManagerContract, shMonadContract } from "./contracts";
-import { CHAIN, eoa, shMonadAddress, taskManagerAddress } from "./constants";
+import { eoa, shMonadAddress, taskManagerAddress } from "./constants";
 import shmonadAbi from "./abi/shmonad.json";
 import taskManagerAbi from "./abi/taskmanager.json";
-import { userClient, publicClient } from "./user";
+import { publicClient, sendTransaction } from "./user";
 import { PolicyBond } from "./types";
 
+//general
+const currentBlockNumber = await publicClient.getBlockNumber();
+console.log("Current block number:", currentBlockNumber);
+
+//Task Manager
 const policyId = await taskManagerContract.read.POLICY_ID();
 console.log("Policy ID:", policyId);
 
 const nonce = await taskManagerContract.read.getAccountNonce([eoa.address]);
 console.log("Nonce:", nonce);
 
+//ShMonad
+//The task manager is an agent (with a policy) in the shMonad contract, so you need to 
+//bond shMON to the policy to schedule tasks
 const depositedAmount = await shMonadContract.read.balanceOf([eoa.address]);
-console.log("Currently deposited amount:", depositedAmount);
+console.log("shMonad Deposited Amount:", depositedAmount);
 
-const bondedAmount = await shMonadContract.read.balanceOfBonded([policyId, eoa.address]) as bigint;
-console.log("Currently bonded amount:", bondedAmount);
+const policyBond = await shMonadContract.read.getPolicyBond([policyId, eoa.address]) as PolicyBond;
+console.log("Policy Unbonding Amount:", policyBond.unbonding);
+console.log("Policy Bonded Amount:", policyBond.bonded);
 
-const currentBlockNumber = await publicClient.getBlockNumber();
-console.log("Current block number:", currentBlockNumber);
-
-const getPolicyBond = await shMonadContract.read.getPolicyBond([policyId, eoa.address]) as PolicyBond;
-console.log("Unbonding Amount:", getPolicyBond.unbonding);
-
+//Schedule a task
+//Scheduling a task to unbond my shMON from the Task Manager Policy, but this could
+//be for activity like dollar cost averaging, recurring payments, etc.
 const unbondAmount = BigInt(10000000000000);
 const data = encodeFunctionData({
     abi: shmonadAbi,
@@ -31,15 +37,7 @@ const data = encodeFunctionData({
     args: [policyId, unbondAmount],
 });
 
-// const hash = await userClient.sendTransaction({
-//     account: eoa,
-//     chain: CHAIN,
-//     to: shMonadAddress,
-//     data: data,
-//   });
-
-// console.log(hash);
-
+//Build the task
 const task = {
     from: eoa.address,
     gas: BigInt(100000),
@@ -48,6 +46,7 @@ const task = {
     nonce,
 }
 
+//Schedule the task
 const schedule = {
     startBlock: currentBlockNumber+BigInt(10),
     interval: BigInt(100),
@@ -56,34 +55,80 @@ const schedule = {
     deadline: currentBlockNumber + BigInt(5000),
 }
 
+//Build the task definition
 const taskDefinition = {
     task,
     schedule,
 }
 
-// const simulate = await taskManagerContract.simulate.scheduleTask([taskDefinition]);
-// console.log(simulate.result);
+//Simulate the task to make sure it works
+const scheduleSimulate  = await taskManagerContract.simulate.scheduleTask([taskDefinition]);
+const taskHash = scheduleSimulate.result;
+console.log("Task Hash:", taskHash);
 
-// const taskData = encodeFunctionData({
-//     abi: taskManagerAbi,
-//     functionName: "scheduleTask",
-//     args: [taskDefinition],
-// });
+//Build the task data
+const scheduleTaskData = encodeFunctionData({
+    abi: taskManagerAbi,
+    functionName: "scheduleTask",
+    args: [taskDefinition],
+});
 
-// const hash = await userClient.sendTransaction({
-//     account: eoa,
-//     chain: CHAIN,
-//     to: taskManagerAddress,
-//     data: taskData,
-//   });
+//Send it!
+const scheduleTaskHash = await sendTransaction(taskManagerAddress, scheduleTaskData);
+console.log("Schedule Tx Hash:", scheduleTaskHash);
 
-// console.log(hash);
-
+//Retrieve your tasks
 const accountTasks = await taskManagerContract.read.getAccountTasks([eoa.address]) as Hex[];
-console.log(accountTasks);
+const lastTaskHash = accountTasks[accountTasks.length-1] as Hex;
+console.log("Last Task Hash:", lastTaskHash);
 
-const taskInfo = await taskManagerContract.read.getTaskInfo([accountTasks[0]]);
+//Schedule the new task
+const newSchedule = {
+    startBlock: currentBlockNumber+BigInt(10),
+    interval: BigInt(1000), //UPDATED INTERVAL TO 1000 blocks
+    executions: 10,
+    active: true,
+    deadline: currentBlockNumber + BigInt(5000),
+}
+
+//Build the new task schedule
+const newTaskDefinition = {
+    task,
+    schedule: newSchedule,
+}
+
+//Update the task schedule
+const updateTaskSchedule = await taskManagerContract.simulate.updateTaskSchedule([lastTaskHash, newTaskDefinition]);
+
+//Build the task data
+const updateTaskScheduleData = encodeFunctionData({
+    abi: taskManagerAbi,
+    functionName: "updateTaskSchedule",
+    args: [lastTaskHash, newTaskDefinition],
+});
+
+//Send it!
+const updateTaskScheduleHash = await sendTransaction(taskManagerAddress, updateTaskScheduleData);
+console.log("Update Task Schedule Tx Hash:", updateTaskScheduleHash);
+
+//Cancel a task
+const cancelSimulate = await taskManagerContract.simulate.cancelTask([lastTaskHash]);
+
+//Build the task data
+const cancelTaskData = encodeFunctionData({
+    abi: taskManagerAbi,
+    functionName: "cancelTask",
+    args: [lastTaskHash],
+});
+
+//Send it!
+const cancelTaskHash = await sendTransaction(taskManagerAddress, cancelTaskData);
+console.log("Cancel Tx Hash:", cancelTaskHash);
+
+//Retrieve the task info
+const taskInfo = await taskManagerContract.read.getTaskInfo([lastTaskHash]);
 console.log(taskInfo);
+
 
 
 
--- src/user.ts
+++ src/user.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, createWalletClient, http } from "viem";
+import { Address, createPublicClient, createWalletClient, Hex, http } from "viem";
 import { CHAIN, rpcUrl, USER_PK } from "./constants";
 import { privateKeyToAccount } from "viem/accounts";
 
@@ -12,5 +12,14 @@ const publicClient = createPublicClient({
   transport: http(rpcUrl),
   chain: CHAIN,
 });
+
+export async function sendTransaction(to: Address, data: Hex) {
+  return await userClient.sendTransaction({
+      account: userClient.account,
+      chain: CHAIN,
+      to: to,
+      data: data,
+    });
+}
 
 export { userClient, publicClient };
